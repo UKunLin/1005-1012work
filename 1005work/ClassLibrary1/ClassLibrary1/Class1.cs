92347d6f5702891c66c9fbeb2c2f79cdd26960f3
kunlin yu <yukunlin10@gmail.com>
2018-10-07 21:56:21 +0800

    1

---
+++ 1005work/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,78 @@
+﻿using ClassLibrary1.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+
+namespace ClassLibrary1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            var nodes=FindOpenData();
+            ShowOpenData(nodes);
+            Console.ReadKey();
+        }
+
+        static List<OpenData> FindOpenData()
+        {
+            List<OpenData> result = new List<OpenData>();
+            var xml = XElement.Load(@"https://apiservice.mol.gov.tw/OdService/download/A17000000J-020104-62d");
+            var nodes = xml.Descendants("row").ToList();
+
+            for(var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+                OpenData item = new OpenData();
+                item.縣市別 = getValue(node, "縣市別");
+                item.應開戶數 = getValue(node, "應開戶數");
+                item.已開戶數 = getValue(node, "已開戶數");
+                result.Add(item);
+            }
+
+
+
+            return result;
+        }
+
+
+        private static string getValue(XElement node,string propertyName)
+        {
+            return node.Element(propertyName)?.Value?.Trim();
+        }
+
+
+        public static void ShowOpenData(List<OpenData> nodes)
+        {
+            Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+            nodes.GroupBy(node => node.縣市別).ToList().ForEach(group =>
+            {
+                var key = group.Key;
+                var groupDatas = group.ToList();
+                var message = $"縣市別:{key},共有{groupDatas.Count()}筆資料";
+                Console.WriteLine(message);
+
+            });
+
+            nodes.GroupBy(node => node.應開戶數).ToList().ForEach(group =>
+            {
+                var key = group.Key;
+                var groupDatas = group.ToList();
+                var message = $"應開戶數:{key},共有{groupDatas.Count()}筆資料";
+                Console.WriteLine(message);
+
+            });
+
+            nodes.GroupBy(node => node.已開戶數).ToList().ForEach(group =>
+            {
+                var key = group.Key;
+                var groupDatas = group.ToList();
+                var message = $"已開戶數:{key},共有{groupDatas.Count()}筆資料";
+                Console.WriteLine(message);
+
+            });
+        }
+    }
+}
---
+++ 1005work/ClassLibrary1/ClassLibrary1/models/OpenData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1.models
+{
+    class OpenData
+    {
+        public string 縣市別 { get; set; }
+        public string 應開戶數 { get; set; }
+        public string 已開戶數 { get; set; }
+      
+    }
+
+    
+}
